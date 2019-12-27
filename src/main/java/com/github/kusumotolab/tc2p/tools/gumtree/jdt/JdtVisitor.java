2f17ee91b6aeae9d4208e03bec905e95824bfe82
mtj0928 <mtj0928@users.noreply.github.com>
2019-12-27 09:43:39 +0900

    merge SimpleName which represents the method name to MethodIvocation

--- src/main/java/com/github/kusumotolab/tc2p/tools/gumtree/jdt/JdtVisitor.java
+++ src/main/java/com/github/kusumotolab/tc2p/tools/gumtree/jdt/JdtVisitor.java
@@ -35,41 +35,78 @@ import org.eclipse.jdt.core.dom.PostfixExpression;
 import org.eclipse.jdt.core.dom.PrefixExpression;
 import org.eclipse.jdt.core.dom.QualifiedName;
 import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.SimplePropertyDescriptor;
 import org.eclipse.jdt.core.dom.StringLiteral;
 import org.eclipse.jdt.core.dom.TagElement;
 import org.eclipse.jdt.core.dom.TextElement;
 import org.eclipse.jdt.core.dom.Type;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
-public class JdtVisitor  extends AbstractJdtVisitor {
+public class JdtVisitor extends AbstractJdtVisitor {
+
     public JdtVisitor() {
         super();
     }
 
     @Override
     public void preVisit(ASTNode n) {
+        if (isMethodName(n)) {
+            return;
+        }
         pushNode(n, getLabel(n));
     }
-
     protected String getLabel(ASTNode n) {
-        if (n instanceof Name) return ((Name) n).getFullyQualifiedName();
-        if (n instanceof Type) return n.toString();
-        if (n instanceof Modifier) return n.toString();
-        if (n instanceof StringLiteral) return ((StringLiteral) n).getEscapedValue();
-        if (n instanceof NumberLiteral) return ((NumberLiteral) n).getToken();
-        if (n instanceof CharacterLiteral) return ((CharacterLiteral) n).getEscapedValue();
-        if (n instanceof BooleanLiteral) return ((BooleanLiteral) n).toString();
-        if (n instanceof InfixExpression) return ((InfixExpression) n).getOperator().toString();
-        if (n instanceof PrefixExpression) return ((PrefixExpression) n).getOperator().toString();
-        if (n instanceof PostfixExpression) return ((PostfixExpression) n).getOperator().toString();
-        if (n instanceof Assignment) return ((Assignment) n).getOperator().toString();
-        if (n instanceof MethodInvocation) return  ((MethodInvocation) n).getName().toString();
-        if (n instanceof TextElement) return n.toString();
-        if (n instanceof TagElement) return ((TagElement) n).getTagName();
-        if (n instanceof TypeDeclaration) return ((TypeDeclaration) n).isInterface() ? "interface" : "class";
+        if (n instanceof Name) {
+            return ((Name) n).getFullyQualifiedName();
+        }
+        if (n instanceof Type) {
+            return n.toString();
+        }
+        if (n instanceof Modifier) {
+            return n.toString();
+        }
+        if (n instanceof StringLiteral) {
+            return ((StringLiteral) n).getEscapedValue();
+        }
+        if (n instanceof NumberLiteral) {
+            return ((NumberLiteral) n).getToken();
+        }
+        if (n instanceof CharacterLiteral) {
+            return ((CharacterLiteral) n).getEscapedValue();
+        }
+        if (n instanceof BooleanLiteral) {
+            return ((BooleanLiteral) n).toString();
+        }
+        if (n instanceof InfixExpression) {
+            return ((InfixExpression) n).getOperator().toString();
+        }
+        if (n instanceof PrefixExpression) {
+            return ((PrefixExpression) n).getOperator().toString();
+        }
+        if (n instanceof PostfixExpression) {
+            return ((PostfixExpression) n).getOperator().toString();
+        }
+        if (n instanceof Assignment) {
+            return ((Assignment) n).getOperator().toString();
+        }
+        if (n instanceof MethodInvocation) {
+            return ((MethodInvocation) n).getName().toString();
+        }
+        if (n instanceof TextElement) {
+            return n.toString();
+        }
+        if (n instanceof TagElement) {
+            return ((TagElement) n).getTagName();
+        }
+        if (n instanceof TypeDeclaration) {
+            return ((TypeDeclaration) n).isInterface() ? "interface" : "class";
+        }
 
         return "";
+    }
+
+    @Override
+    public boolean visit(final SimpleName node) {
+        return false;
     }
 
     @Override
@@ -84,6 +121,16 @@ public class JdtVisitor  extends AbstractJdtVisitor {
 
     @Override
     public void postVisit(ASTNode n) {
+        if (isMethodName(n)) {
+            return;
+        }
         popNode();
     }
+
+    private boolean isMethodName(final ASTNode n) {
+        return n instanceof SimpleName
+            && n.getParent() instanceof MethodInvocation
+            && ((MethodInvocation) n.getParent()).getName().equals(n);
+    }
+
 }
